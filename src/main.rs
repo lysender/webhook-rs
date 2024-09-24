376f555e25c153d1b5d797d36f2358c512b49984
Lysender <lysender@darkstar-win>
2024-09-24 13:14:35 +0800

    fix: use simple routes

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 use std::{
-    fs,
     io::{prelude::*, BufReader},
     net::{TcpListener, TcpStream},
 };
@@ -29,12 +28,12 @@ fn handle_connection(mut stream: TcpStream) {
 
     println!("{}", request_line);
 
-    let (status_line, filename) = match request_line.as_str() {
-        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "templates/hello.html"),
-        _ => ("HTTP/1.1 404 NOT FOUND", "templates/404.html"),
+    let (status_line, contents) = match request_line.as_str() {
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "OK"),
+        "GET /webhook HTTP/1.1" => ("HTTP/1.1 200 OK", "OK"),
+        _ => ("HTTP/1.1 404 NOT FOUND", "Not Found"),
     };
 
-    let contents = fs::read_to_string(filename).unwrap();
     let length = contents.len();
 
     let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
