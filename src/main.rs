24c8c78a4fe4a0267895583c76a656482397df30
Lysender <lysender@darkstar-win>
2024-09-24 13:19:48 +0800

    fix: move web server

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,8 @@
-use std::{
-    io::{prelude::*, BufReader},
-    net::{TcpListener, TcpStream},
-};
-
+mod web;
 mod workers;
 
-use workers::ThreadPool;
+use web::start_web_server;
 
 fn main() {
-    let port = "7878";
-    let address = format!("127.0.0.1:{}", port);
-    let listener = TcpListener::bind(address.as_str()).unwrap();
-    let pool = ThreadPool::new(4);
-
-    for stream in listener.incoming() {
-        let st = stream.unwrap();
-
-        pool.execute(|| {
-            handle_connection(st);
-        });
-    }
-}
-
-fn handle_connection(mut stream: TcpStream) {
-    let reader = BufReader::new(&mut stream);
-    let request_line = reader.lines().next().unwrap().unwrap();
-
-    println!("{}", request_line);
-
-    let (status_line, contents) = match request_line.as_str() {
-        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "OK"),
-        "GET /webhook HTTP/1.1" => ("HTTP/1.1 200 OK", "OK"),
-        _ => ("HTTP/1.1 404 NOT FOUND", "Not Found"),
-    };
-
-    let length = contents.len();
-
-    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
-    stream.write_all(response.as_bytes()).unwrap();
+    start_web_server();
 }
---
+++ src/web.rs
@@ -0,0 +1,40 @@
+use std::{
+    io::{prelude::*, BufReader},
+    net::{TcpListener, TcpStream},
+};
+
+use crate::workers::ThreadPool;
+
+pub fn start_web_server() {
+    let port = "7878";
+    let address = format!("127.0.0.1:{}", port);
+    let listener = TcpListener::bind(address.as_str()).unwrap();
+    let pool = ThreadPool::new(4);
+
+    for stream in listener.incoming() {
+        let st = stream.unwrap();
+
+        pool.execute(|| {
+            handle_connection(st);
+        });
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let reader = BufReader::new(&mut stream);
+    let request_line = reader.lines().next().unwrap().unwrap();
+
+    println!("{}", request_line);
+
+    let (status_line, contents) = match request_line.as_str() {
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "OK"),
+        "GET /webhook HTTP/1.1" => ("HTTP/1.1 200 OK", "OK"),
+        "POST /webhook HTTP/1.1" => ("HTTP/1.1 200 OK", "OK"),
+        _ => ("HTTP/1.1 404 NOT FOUND", "Not Found"),
+    };
+
+    let length = contents.len();
+
+    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+    stream.write_all(response.as_bytes()).unwrap();
+}
