479df9c847bc05b4023750f1cd3da25a7e02023b
Lysender <lysender@darkstar-win>
2024-09-27 10:59:41 +0800

    fix: initial setup for client merge

--- src/config.rs
+++ src/config.rs
@@ -4,6 +4,7 @@ use std::{
     fs,
     path::{Path, PathBuf},
 };
+use url::Url;
 
 use crate::{utils::valid_webhook_path, Error, Result};
 
@@ -14,6 +15,13 @@ pub struct ServerConfig {
     pub web_port: u16,
     pub tunnel_port: u16,
     pub webhook_path: String,
+    pub jwt_secret: String,
+}
+
+#[derive(Clone, Deserialize)]
+pub struct ClientConfig {
+    pub tunnel_address: String,
+    pub target_url: String,
     pub jwt_secret: String,
 }
 
@@ -40,6 +48,48 @@ impl ServerConfig {
         if !valid_webhook_path(config.webhook_path.as_str()) {
             return Err(Error::ConfigInvalidError(
                 "Webhook path must be in this format: /foo-bar".to_string(),
+            ));
+        }
+
+        if config.jwt_secret.len() == 0 {
+            return Err(Error::ConfigInvalidError(
+                "JWT secret must no be empty.".to_string(),
+            ));
+        }
+
+        Ok(config)
+    }
+}
+
+impl ClientConfig {
+    pub fn build(filename: &Path) -> Result<ClientConfig> {
+        let toml_string = match fs::read_to_string(filename) {
+            Ok(str) => str,
+            Err(error) => {
+                return Err(Error::ConfigReadError(error.to_string()));
+            }
+        };
+
+        let config: ClientConfig = match toml::from_str(toml_string.as_str()) {
+            Ok(value) => value,
+            Err(error) => return Err(Error::ConfigParseError(error.to_string())),
+        };
+
+        if config.tunnel_address.len() == 0 {
+            return Err(Error::ConfigInvalidError(
+                "Tunnel address must not be empty.".to_string(),
+            ));
+        }
+
+        if config.target_url.len() == 0 {
+            return Err(Error::ConfigInvalidError(
+                "Target application URL must not be empty.".to_string(),
+            ));
+        }
+
+        if let Err(_) = Url::parse(config.target_url.as_str()) {
+            return Err(Error::ConfigInvalidError(
+                "Target application URL must be a valid URL.".to_string(),
             ));
         }
 
